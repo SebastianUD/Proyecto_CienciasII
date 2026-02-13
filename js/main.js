3592a2dcc2e415c225f6826cf214d8344312b8c6
SebastianUD <112674146+SebastianUD@users.noreply.github.com>
2026-02-12 21:53:40 -0500

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import { AppController } from './controllers/AppController.js';
-
-// Punto de entrada de la aplicación (Main)
-// Esperamos a que el DOM esté completamente cargado antes de ejecutar lógica.
-document.addEventListener('DOMContentLoaded', () => {
-    // Instanciamos el controlador principal que orquesta todo.
-    const app = new AppController();
-    app.iniciar();
-});
